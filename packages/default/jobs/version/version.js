757b25116dc953e25dcf7c4f8bc4ff602f5d7984
Haiylu <Haiylu.Kibret@gmail.com>
2018-11-09 22:23:51 -0500

    Update version.js

--- packages/default/jobs/version/version.js
+++ packages/default/jobs/version/version.js
@@ -30,6 +30,7 @@ let created_at='created_at":"2018-10-31T15:02:15Z"';
 function callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
+    var version=info.version;
    //var project=[];
    jobCallback(null, {url: url,version:version});
     /*
